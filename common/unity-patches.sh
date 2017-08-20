f3a0a2a5fd93a2489cbbd88b1e0637519cb17e32
ahrion <Ahrionmgallegos@gmail.com>
2017-08-20 15:48:45 -0600

    add conditional statements instead of sed removing lines

--- common/unity-patches.sh
+++ common/unity-patches.sh
@@ -6,15 +6,9 @@
 #
 #
 # ^ DO NOT MODIFY ^
-for CFG in $CONFIG_FILE $HTC_CONFIG_FILE $OTHER_V_FILE $OFFLOAD_CONFIG $V_CONFIG_FILE; do
-  if [ -f $CFG ]; then
-    sed -i '/jamesdsp {/,/}/d' $UNITYPATCH$CFG
-    sed -i '/jdsp {/,/}/d' $UNITYPATCH$CFG
-  fi
-done
 ui_print "    Patching existing audio_effects files..."
 for CFG in $CONFIG_FILE $HTC_CONFIG_FILE $OTHER_V_FILE $OFFLOAD_CONFIG $V_CONFIG_FILE; do
-  if [ -f $CFG ]; then
+  if [ -f $CFG ] && [ ! "$(cat $AMLPATH$CFG | grep ' jamesdsp {')" ]; then
     sed -i 's/^effects {/effects {\n  jamesdsp {\n    library jdsp\n    uuid f27317f4-c984-4de6-9a90-545759495bf2\n  }/g' $UNITYPATCH$CFG
     sed -i 's/^libraries {/libraries {\n  jdsp {\n    path \/system\/lib\/soundfx\/libjamesdsp.so\n  }/g' $UNITYPATCH$CFG
   fi
