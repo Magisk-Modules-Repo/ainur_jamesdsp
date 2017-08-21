d74e39671591e418ef4021b9e387d942c6470be7
Zackptg5 <Zackptg5@comcast.net>
2017-08-21 19:45:47 -0400

    Add changes from v4a

--- common/unity-patches-remove.sh
+++ common/unity-patches-remove.sh
@@ -1,14 +1,14 @@
 # v DO NOT MODIFY v
 # See instructions file for predefined variables
 # Add patches (such as audio config) here (starting at line 9)
-# NOTE: Destination variable must have '$UNITYPATCH' in front of it
-# Patch Ex: sed -i '/v4a_standard_fx {/,/}/d' $UNITYPATCH$CONFIG_FILE
+# NOTE: Destination variable must have '$AMLPATCH' in front of it
+# Patch Ex: sed -i '/v4a_standard_fx {/,/}/d' $AMLPATCH$CONFIG_FILE
 #
 #
 # ^ DO NOT MODIFY ^
 for CFG in $CONFIG_FILE $HTC_CONFIG_FILE $OTHER_V_FILE $OFFLOAD_CONFIG $V_CONFIG_FILE; do
   if [ -f $CFG ]; then
-    sed -i '/jamesdsp {/,/}/d' $UNITYPATCH$CFG
-    sed -i '/jdsp {/,/}/d' $UNITYPATCH$CFG
+    sed -i '/jamesdsp {/,/}/d' $AMLPATCH$CFG
+    sed -i '/jdsp {/,/}/d' $AMLPATCH$CFG
   fi
 done
