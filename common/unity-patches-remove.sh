59c036ca65ce6ff9b4f7155b0f8fa8fa73f4e25e
Zackptg5 <Zackptg5@comcast.net>
2017-08-22 08:17:53 -0400

    Typo fix

--- common/unity-patches-remove.sh
+++ common/unity-patches-remove.sh
@@ -1,14 +1,14 @@
 # v DO NOT MODIFY v
 # See instructions file for predefined variables
 # Add patches (such as audio config) here (starting at line 9)
-# NOTE: Destination variable must have '$AMLPATCH' in front of it
-# Patch Ex: sed -i '/v4a_standard_fx {/,/}/d' $AMLPATCH$CONFIG_FILE
+# NOTE: Destination variable must have '$AMLPATH' in front of it
+# Patch Ex: sed -i '/v4a_standard_fx {/,/}/d' $AMLPATH$CONFIG_FILE
 #
 #
 # ^ DO NOT MODIFY ^
 for CFG in $CONFIG_FILE $HTC_CONFIG_FILE $OTHER_V_FILE $OFFLOAD_CONFIG $V_CONFIG_FILE; do
   if [ -f $CFG ]; then
-    sed -i '/jamesdsp {/,/}/d' $AMLPATCH$CFG
-    sed -i '/jdsp {/,/}/d' $AMLPATCH$CFG
+    sed -i '/jamesdsp {/,/}/d' $AMLPATH$CFG
+    sed -i '/jdsp {/,/}/d' $AMLPATH$CFG
   fi
 done
--- common/unity-patches.sh
+++ common/unity-patches.sh
@@ -1,15 +1,15 @@
 # v DO NOT MODIFY v
 # See instructions file for predefined variables
 # Add patches (such as audio config) here (starting at line 9)
-# NOTE: Destination variable must have '$AMLPATCH' in front of it
-# Patch Ex: sed -i '/v4a_standard_fx {/,/}/d' $AMLPATCH$CONFIG_FILE
+# NOTE: Destination variable must have '$AMLPATH' in front of it
+# Patch Ex: sed -i '/v4a_standard_fx {/,/}/d' $AMLPATH$CONFIG_FILE
 #
 #
 # ^ DO NOT MODIFY ^
 ui_print "    Patching existing audio_effects files..."
 for CFG in $CONFIG_FILE $HTC_CONFIG_FILE $OTHER_V_FILE $OFFLOAD_CONFIG $V_CONFIG_FILE; do
   if [ -f $CFG ] && [ ! "$(cat $AMLPATH$CFG | grep ' jamesdsp {')" ]; then
-    sed -i 's/^effects {/effects {\n  jamesdsp {\n    library jdsp\n    uuid f27317f4-c984-4de6-9a90-545759495bf2\n  }/g' $AMLPATCH$CFG
-    sed -i 's/^libraries {/libraries {\n  jdsp {\n    path \/system\/lib\/soundfx\/libjamesdsp.so\n  }/g' $AMLPATCH$CFG
+    sed -i 's/^effects {/effects {\n  jamesdsp {\n    library jdsp\n    uuid f27317f4-c984-4de6-9a90-545759495bf2\n  }/g' $AMLPATH$CFG
+    sed -i 's/^libraries {/libraries {\n  jdsp {\n    path \/system\/lib\/soundfx\/libjamesdsp.so\n  }/g' $AMLPATH$CFG
   fi
 done
