f0f8ec06947f373e42eb39e2f50138e059cd5afd
Zackptg5 <zackptg5@comcast.net>
2017-09-21 14:48:38 -0400

    Instructions updates, add proper app wiping

--- common/unity-files-wipe.sh
+++ common/unity-files-wipe.sh
@@ -2,10 +2,8 @@
 # See instructions file for predefined variables
 # Add files/folders (full paths) to remove here (Ex: $SYS/etc/audmodlib/audmodlib.conf)
 # If folder, add FOL prefix (Ex: FOL$SYS/etc/audmodlib)
-# Directories for files will be removed if empty after file removal
-# For apps, use APP variables in reference file
-# Apps must always be in this format: $SYS/$APPPATH1/$APP1.apk
+# If app, just put the app name with the APP prefix (Ex: APPViPER4AndroidFX) - note the lack of slashes and the '.apk' extension
 # ^ DO NOT MODIFY ^
 $SYS/lib/soundfx/libjamesDSPImpulseToolbox.so
 $SYS/lib/soundfx/libjamesdsp.so
-$SYS/$APPPATH1/$APP1.apk
+APPJamesDSPManager
