6be4a9926d3804b8ab0b61e834cbd3db507b10be
Zackptg5 <zackptg5@comcast.net>
2017-10-05 23:09:21 -0400

    Remove old mkdir stuff

--- common/unity-customrules1.sh
+++ common/unity-customrules1.sh
@@ -5,7 +5,7 @@
 # You can create copies of this file and name is the same as this but with the next number after it (ex: unity-customrules2.sh)
 # See instructions for TIMEOFEXEC values, do not remove it
 # ^ DO NOT MODIFY ^
-TIMEOFEXEC=3
+TIMEOFEXEC=2
 if [ "$ABI" == "arm" ]; then
   ui_print "    Installing libs for arm/arm64 device"
   $CP_PRFX $INSTALLER/custom/lib/arm/libjamesdsp.so $UNITY$SYS/lib/soundfx/libjamesdsp.so$CP_SFFX
